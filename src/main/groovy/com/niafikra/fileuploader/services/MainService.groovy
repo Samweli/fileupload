f9d36645f2bac37b830a62fe914188761e9d9624
Samweli <smwltwesa6@gmail.com>
2014-08-29 16:36:34 +0300

    added services file

---
+++ src/main/groovy/com/niafikra/fileuploader/services/MainService.groovy
@@ -0,0 +1,11 @@
+package com.niafikra.fileuploader.services
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: samweli
+ * Date: 8/29/14
+ * Time: 4:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MainService {
+}
